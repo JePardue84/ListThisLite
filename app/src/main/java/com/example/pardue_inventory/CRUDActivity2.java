ce37fb2eefdd9dc4a9c7b08db7b78591bcffca91
Joshua Pardue <JePardue84@gmail.com>
2022-11-19 08:37:10 -0600

    List This! App

---
+++ app/src/main/java/com/example/pardue_inventory/CRUDActivity2.java
@@ -0,0 +1,14 @@
+package com.example.pardue_inventory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CRUDActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crudactivity2);
+    }
+}
